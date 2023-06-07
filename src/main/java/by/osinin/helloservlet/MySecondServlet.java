e8b270fd6a32de7331452eaf3bfd3c97d2334a75
Epson <A.osinin.epson@gmail.com>
2023-06-07 21:55:37 +0300

    classwork 21

--- src/main/java/by/osinin/helloservlet/MySecondServlet.java
+++ src/main/java/by/osinin/helloservlet/MySecondServlet.java
@@ -1,18 +1,68 @@
 package by.osinin.helloservlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.Collectors;
 
-public class MyFirstServlet extends HttpServlet {
+@WebServlet(value = "/hi/*")
+public class MySecondServlet extends HttpServlet {
+    private int counter;
+    @Override
+    public void init() throws ServletException {
+        counter = 100;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet path: " + req.getServletPath());
+        System.out.println("Servlet URI: " + req.getRequestURI());
+
+        //http://localhost:8080/hi/123?my=parametr&another=empty
+        System.out.println("Paramentr: " + req.getParameter("my"));
+
+        HttpSession session = req.getSession();
+        Integer currenParam = (Integer) session.getAttribute("count");
+        System.out.println("Current param: " + currenParam);
+        if (currenParam == null) {
+            currenParam = counter;
+        }
+        session.setAttribute("count", currenParam+1);
+
+
+
         PrintWriter writer = resp.getWriter();
-        writer.println("This is my response");
+        writer.println("This is my second response" + ". Current param: " + currenParam);
+        Iterator<String> headersIterator = req.getHeaderNames().asIterator();
+        while (headersIterator.hasNext()) {
+            writer.println("\nHeaders: " + headersIterator.next());
+        }
+        writer.println("\nHost: " + req.getHeader("host"));
+        resp.setHeader("myHeader", "abracadabra");
+        writer.println("\nCookies: " + Arrays.asList(req.getCookies())
+                .stream()
+                .map(cookie -> cookie.getName()+ " " + cookie.getValue() + " " + cookie.getMaxAge())
+                .collect(Collectors.toList()));
+
+
+
+        if (currenParam>102) {
+            resp.sendRedirect("/here");
+            Person person = new Person();
+            //resp.setStatus(500);
+            //resp.sendError(500, "Too much");
+        } //ошибку сделать
+
+
+        //writer.println("\nHeaders: " + req.getHeaderNames());
 
     }
 }
--- src/main/java/by/osinin/helloservlet/Person.java
+++ src/main/java/by/osinin/helloservlet/Person.java
@@ -1,2 +1,15 @@
-package by.osinin.helloservlet;public class Person {
+package by.osinin.helloservlet;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Data //что бы вставить все конструкторы (геттеры, сеттеры и т.д.)
+@NoArgsConstructor
+public class Person {
+    private int age;
+    private String name;
+    private String login;
+    private String password;
+
+
 }
